8ca3660d8d40ce92728067298f2e19cfef494021
Grim-redd <mainasara.olulanke@stu.cu.edu.ng>
2023-06-22 19:25:08 +0100

    Added a more sophisticated carousel 😮‍💨

--- components/Home-Page/Section1.tsx
+++ components/Home-Page/Section1.tsx
@@ -1,63 +1,86 @@
 import Image from "next/image";
 
 import { carouselSections } from "@/static-data/images";
-import { motion } from "framer-motion";
-import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import React, { useState } from "react";
 import { FaArrowRight } from "react-icons/fa";
 
 import LeftArrow from "../UI/Carousel/LeftArrow";
 import RightArrow from "../UI/Carousel/RightArrow";
-import useFullWidthCarousel from "../UI/Carousel/useFullWidthCarousel";
 
 const Section1 = () => {
-  const { controls, prevSlide, nextSlide } =
-    useFullWidthCarousel(carouselSections);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [next, setNext] = useState(true);
+
+  const variants = {
+    hidden: {
+      x: next ? 200 : -200,
+      opacity: 0,
+    },
+    visible: {
+      x: 0,
+      opacity: 1,
+    },
+  };
+
+  function nextSlide() {
+    setNext(true);
+
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % carouselSections.length);
+  }
+
+  function prevSlide() {
+    setNext(false);
+
+    setCurrentSlide(
+      (prevSlide) =>
+        (carouselSections.length + prevSlide - 1) % carouselSections.length
+    );
+  }
 
   return (
     <section className="relative w-full h-screen overflow-scroll">
-      <LeftArrow onclick={prevSlide} />
+      {currentSlide > 0 ? <LeftArrow onclick={prevSlide} /> : null}
 
-      <motion.div animate={controls} className="flex h-full bg-black w-fit">
-        {carouselSections.map((carousel) => {
-          return (
-            <div
-              key={carousel.id}
-              className="relative w-screen h-screen text-white"
-            >
-              <div className="relative w-full h-full">
-                <Image
-                  src={carousel.imgsrc}
-                  fill
-                  placeholder="blur"
-                  alt="Carousel"
-                />
-              </div>
+      <AnimatePresence>
+        <motion.div
+          key={carouselSections[currentSlide].id}
+          variants={variants}
+          initial="hidden"
+          animate="visible"
+          className="relative w-full h-full text-white"
+        >
+          <div className="relative w-full h-full">
+            <Image
+              src={carouselSections[currentSlide].imgsrc}
+              fill
+              placeholder="blur"
+              alt="Carousel"
+            />
+          </div>
 
-              <div className="absolute inset-0 pt-8 pl-5 backdrop-brightness-[30%]">
-                <div className="space-y-3">
-                  <p>FEELING GOOD TODAY</p>
-                  <h1 className="text-9xl">{carousel.location}</h1>
-                </div>
-
-                <div className="absolute max-w-lg space-y-5 bottom-8 right-5">
-                  <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Hic repellendus tempora reiciendis eos esse porro vel quam
-                    commodi pariatur similique eveniet sit sequi vitae rem,
-                    adipisci tenetur exercitationem fugit! Veniam.
-                  </p>
-
-                  <button className="flex items-center gap-x-2">
-                    Learn more <FaArrowRight />
-                  </button>
-                </div>
-              </div>
+          <div className="absolute inset-0 pt-8 pl-5 backdrop-brightness-50">
+            <div className="space-y-3">
+              <p className="text-2xl opacity-75">{carouselSections[currentSlide].subText}</p>
+              <h1 className="max-w-3xl text-7xl">
+                {carouselSections[currentSlide].mainText}
+              </h1>
             </div>
-          );
-        })}
-      </motion.div>
 
-      <RightArrow onclick={nextSlide} />
+            <div className="absolute max-w-lg space-y-5 bottom-8 right-5">
+              <p className="text-lg">
+                {carouselSections[currentSlide].description}
+              </p>
+
+              <button className="flex items-center gap-x-2">
+                Learn more <FaArrowRight />
+              </button>
+            </div>
+          </div>
+        </motion.div>
+      </AnimatePresence>
+
+      {currentSlide < carouselSections.length - 1 ? <RightArrow onclick={nextSlide} /> : null}
     </section>
   );
 };
--- static-data/images.js
+++ static-data/images.js
@@ -31,31 +31,41 @@ export const carouselSections = [
   {
     id: 1,
     imgsrc: Carousel1,
-    location: "Maldives",
-    description: "",
+    mainText: "Set Sail on a Luxury Cruise",
+    subText: "Experience Unmatched Comfort and Adventure",
+    description:
+      "Embark on a journey of a lifetime aboard our exquisite cruise ships. Enjoy luxurious amenities, breathtaking views, and a wide range of activities for an unforgettable vacation.",
   },
   {
     id: 2,
     imgsrc: Carousel2,
-    location: "Spa",
-    description: "",
+    mainText: "Indulge in Tranquil Spa Retreats",
+    subText: "Revitalize Your Mind, Body, and Soul",
+    description:
+      "Escape to a world of serenity and relaxation with our rejuvenating spa retreats. Unwind amidst soothing ambiance, skilled therapists, and pampering treatments that will leave you feeling refreshed.",
   },
   {
     id: 3,
     imgsrc: Carousel3,
-    location: "Diving",
-    description: "",
+    mainText: "Embrace the Thrill of Skydiving",
+    subText: "Soar Above the Clouds and Feel Alive",
+    description:
+      "Experience the ultimate adrenaline rush with our exhilarating skydiving adventures. Dive from high above, feel the rush of wind, and witness awe-inspiring views as you conquer the skies.",
   },
   {
     id: 4,
     imgsrc: Carousel4,
-    location: "Serenity",
-    description: "",
+    mainText: "Unwind on Secluded Beaches",
+    subText: "Escape to Tranquility Under the Moonlight",
+    description:
+      "Immerse yourself in the enchanting beauty of our secluded beaches. Take romantic strolls along the shoreline, bask in the gentle ocean breeze, and witness the mesmerizing spectacle of the night sky.",
   },
   {
     id: 5,
     imgsrc: Carousel5,
-    location: "Climbing",
-    description: "",
+    mainText: "Conquer Majestic Peaks",
+    subText: "Reach New Heights, Unleash Your Adventure",
+    description:
+      "Embark on a thrilling mountain climbing expedition and conquer majestic peaks. Push your limits, revel in breathtaking vistas, and savor the sense of achievement at the pinnacle of the world.",
   },
 ];
