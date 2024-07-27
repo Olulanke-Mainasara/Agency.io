bc3f93e4cfb86a00f68b171047ee6c103a1084ee
Grim-redd <mainasara.o.olulanke@gmail.com>
2024-07-27 18:37:53 +0100

    Fixed the bugs in the useLocation hook and the currency converter form💯

--- app/utilities/currency-converter/page.tsx
+++ app/utilities/currency-converter/page.tsx
@@ -24,6 +24,7 @@ const Currency = () => {
   };
 
   const convertCurrency = async () => {
+    setResult("Converting...");
     const response = await fetch(
       `https://v6.exchangerate-api.com/v6/${process.env.NEXT_PUBLIC_EXCHANGE_RATE_API_KEY}/latest/${fromCurrency}`,
       { cache: "no-store" }
@@ -31,7 +32,11 @@ const Currency = () => {
     const data = await response.json();
     const exchangeRate = data.conversion_rates[toCurrency];
     const convertedAmount = amount * exchangeRate;
-    setResult(`${convertedAmount.toFixed(2)} ${toCurrency}`);
+    const formattedAmount = convertedAmount.toLocaleString(undefined, {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+    setResult(`${formattedAmount} ${toCurrency}`);
   };
 
   React.useEffect(() => {
@@ -40,13 +45,13 @@ const Currency = () => {
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-5">
-      <p className="flex w-[90%] max-w-lg justify-between rounded-xl border border-brandDark px-5 py-8 text-5xl dark:border-brandLight md:text-6xl">
+    <main className="flex flex-col items-center justify-center min-h-screen gap-5">
+      <p className="flex w-[90%] max-w-lg justify-between rounded-xl border border-brandDark px-5 py-8 text-4xl dark:border-brandLight md:text-5xl">
         {result}
       </p>
 
-      <div className="flex w-[90%] max-w-lg space-x-5">
-        <div className="flex w-full items-center justify-between space-x-2 rounded-lg border p-3">
+      <div className="flex w-[90%] max-w-lg items-center space-x-5">
+        <div className="flex flex-col items-center justify-between w-full p-3 border rounded-lg sm:flex-row sm:space-x-2">
           <label htmlFor="from-currency" className="w-full text-xl">
             From:
           </label>
@@ -62,7 +67,7 @@ const Currency = () => {
             id="from-currency"
             value={fromCurrency}
             onChange={(e) => handleCurrencyChange(e.target.value, "from")}
-            className="w-full border-brandDark text-xl outline-none dark:border-brandLight"
+            className="w-full text-xl outline-none border-brandDark dark:border-brandLight"
           >
             {currencies.map((currency: string) => (
               <option key={currency} value={currency}>
@@ -74,7 +79,7 @@ const Currency = () => {
 
         <FaExchangeAlt size={60} className="text-3xl text-brandLight" />
 
-        <div className="flex w-full items-center justify-between space-x-2 rounded-lg border p-3">
+        <div className="flex flex-col items-center justify-between w-full p-3 border rounded-lg sm:flex-row sm:space-x-2">
           <label htmlFor="to-currency" className="w-full text-xl">
             To:
           </label>
@@ -90,7 +95,7 @@ const Currency = () => {
             id="to-currency"
             value={toCurrency}
             onChange={(e) => handleCurrencyChange(e.target.value, "to")}
-            className="w-full border-brandDark text-xl outline-none dark:border-brandLight"
+            className="w-full text-xl outline-none border-brandDark dark:border-brandLight"
           >
             {currencies.map((currency) => (
               <option key={currency} value={currency}>
@@ -101,8 +106,8 @@ const Currency = () => {
         </div>
       </div>
 
-      <div className="flex w-[90%] max-w-lg items-center justify-between rounded-lg border p-3">
-        <label htmlFor="amount" className="text-xl">
+      <div className="flex h-14 w-[90%] max-w-lg items-center justify-between overflow-hidden rounded-lg border">
+        <label htmlFor="amount" className="py-3 pl-3 text-xl">
           Amount:
         </label>
         <input
@@ -110,12 +115,13 @@ const Currency = () => {
           id="amount"
           value={amount}
           onChange={(e) => setAmount(Number(e.target.value))}
-          className="border-brandDark text-center text-xl outline-none dark:border-brandLight"
+          className="h-full text-xl text-center bg-transparent border-l outline-none border-brandDark dark:border-brandLight sm:w-1/2"
         />
       </div>
       <button
         onClick={convertCurrency}
-        className="w-[90%] max-w-lg rounded-lg bg-brandDark p-3 text-xl transition-colors hover:bg-brandDark/50"
+        disabled={result === "Converting..."}
+        className="w-[90%] max-w-lg rounded-lg bg-brandDark p-3 text-xl transition-colors hover:bg-brandDark/50 disabled:opacity-50"
       >
         Convert
       </button>
